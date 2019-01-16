2c836de9fa84d8904faef8119c20eb0a3d423b2d
jiaren23 <46737129+jiaren23@users.noreply.github.com>
2019-01-16 17:53:16 +0800

    Add files via upload
    upload folders

---
+++ assets/js/jiaren.js
@@ -0,0 +1,33 @@
+$(document).ready(function(){
+    $("#html").hover(function(){
+        $(".htmlp").fadeToggle(600);
+    })
+
+    $("#css").hover(function(){
+        $(".cssp").fadeToggle(600);
+    })
+
+    $("#js").hover(function(){
+        $(".jsp").fadeToggle(600);
+    })
+
+    $("#androidapp").hover(function(){
+        $(".androidappp").fadeToggle(600);
+    })
+
+    $("#xd").hover(function(){
+        $(".xdp").fadeToggle(600);
+    })
+
+    $("#ai").hover(function(){
+        $(".aip").fadeToggle(600);
+    })		
+
+    $("#ps").hover(function(){
+        $(".psp").fadeToggle(600);
+    })
+
+    $("#pr").hover(function(){
+        $(".prp").fadeToggle(600);
+    })
+})
